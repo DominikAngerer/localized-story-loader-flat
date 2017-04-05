eb5eb4e78f270eb10c538d8d3560ebb5ba347c77
DominikAngerer <me@dominikangerer.com>
2017-04-05 10:25:20 +0200

    fixes different slug for EN and DE

--- index.js
+++ index.js
@@ -15,17 +15,21 @@ module.exports = {
 
     var _locale = 'de';
     if (this.$parent.$parent.$parent.$get('model') && this.$parent.$parent.$parent.$get('model').story) {
-      var _locale = this.$parent.$parent.$parent.$get('model').story.full_slug.slice(0, 2);
+      _locale = this.$parent.$parent.$parent.$get('model').story.full_slug.slice(0, 2);
     }
     if (['de', 'en'].indexOf(_locale) < 0) {
       _locale = 'de';
     }
     if (!this.schema.options) {
-      console.error('localized-story-loader-flat: Define the following options: 0 : token, 1 : starts_with');
+      console.error('localized-story-loader-flat: Define the following options: 0 : token, 1 : starts_with, 2 : en_starts_with');
       return false;
     }
+    var url = 'https://api.storyblok.com/v1/cdn/stories/?token=' + this.schema.options[0].value + '&starts_with=' + _locale + '/' + this.schema.options[1].value + '/&per_page=250&is_startpage=false&time=' + Date.now();
+    if(this.schema.options[2] && _locale == 'en') {
+      url = 'https://api.storyblok.com/v1/cdn/stories/?token=' + this.schema.options[0].value + '&starts_with=' + _locale + '/' + this.schema.options[2].value + '/&per_page=250&is_startpage=false&time=' + Date.now();
+    }
     jQuery.ajax({
-      url: 'https://api.storyblok.com/v1/cdn/stories/?token=' + this.schema.options[0].value + '&starts_with=' + _locale + '/' + this.schema.options[1].value + '/&is_startpage=false&time=' + Date.now(),
+      url: url,
       success: function success(response) {
         _this.$set('data.loadedStories', response.stories);
       }
